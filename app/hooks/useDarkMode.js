6204823c8026299aa2368e64e17aa0b7de2cb6ca
HThanh-how <thanhlt1235@gmail.com>
2024-10-29 17:09:34 +0700

    Add

--- app/hooks/useDarkMode.js
+++ app/hooks/useDarkMode.js
@@ -3,18 +3,24 @@ import { useState, useEffect } from 'react';
 
 const useDarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
-    // Lấy trạng thái từ localStorage hoặc mặc định theo cài đặt hệ điều hành
-    const savedMode = localStorage.getItem('darkMode');
-    return savedMode === 'true' || (savedMode === null && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    // Check if running in the browser
+    if (typeof window !== 'undefined') {
+      // Lấy trạng thái từ localStorage hoặc mặc định theo cài đặt hệ điều hành
+      const savedMode = localStorage.getItem('darkMode');
+      return savedMode === 'true' || (savedMode === null && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }
+    return false; // Default to false if not in the browser
   });
 
   useEffect(() => {
     // Lưu trạng thái vào localStorage
-    localStorage.setItem('darkMode', String(isDarkMode));
-    if (isDarkMode) {
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('darkMode', String(isDarkMode));
+      if (isDarkMode) {
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
     }
   }, [isDarkMode]);
 
